456b50d17784d84d9db4ea9af44df1c8db4b4e97
Trainer <praveen.trainer@outlook.com>
2023-11-21 10:53:46 +0530

    Added css for header.

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,5 +1,19 @@
-export default function Header(){
-    return <header>
-        Application Header
+import Link from "next/link";
+
+export default function Header() {
+    return <header className="bg-gray-800 text-white p-4">
+        <div className="container mx-auto flex justify-between items-center">
+            <h1><Link href="/">Neighborhood Watch</Link></h1>
+            <nav>
+                <ul className="flex space-x-4">
+                    <li>
+                        <Link href="/report-incidents">Incidents</Link>
+                    </li>
+                    <li>
+                        <Link href="/request-service">Services</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </header>
 }
