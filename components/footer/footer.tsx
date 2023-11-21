31cbc29cec280237c68130488a0f0ce09058f086
Trainer <praveen.trainer@outlook.com>
2023-11-21 10:56:26 +0530

    Update footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,5 +1,5 @@
 export default function Footer() {
-    return <footer>
-        Application Footer
+    return <footer className="bg-gray-700 text-white p-4 text-center">
+        <p>&copy; {new Date().getFullYear()} Neighborhood Watch</p>
     </footer>
 }
